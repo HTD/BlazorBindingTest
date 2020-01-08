dcd80ad31acbf56ef305c4a2c8e7155901360f66
Adam Łyskawa <adam@codedog.pl>
2020-01-08 08:29:35 +0100

    Add project files.

---
+++ wwwroot/js/app.js
@@ -0,0 +1,16 @@
+﻿class App {
+
+    constructor(backendReference) {
+        this.backendReference = backendReference;
+        setInterval(() => this.tick.call(this), 1000);
+    }
+
+    tick() {
+        this.backendReference.invokeMethodAsync("OnJsTickOccured");
+    }
+
+}
+
+window.app = function (backendReference) {
+    window.app = new App(backendReference);
+}
